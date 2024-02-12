fc997d0dc34166599e46b6adeb6e329bc217d8aa
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-02-12 21:40:26 +0500

    feat: solve snapshot tests

---
+++ src/08-snapshot-testing/data.ts
@@ -0,0 +1,11 @@
+type Movie = { id: number; name: string; rating: number };
+export const moviesList1: Movie[] = [
+  { id: 1, name: 'Interstellar', rating: 10 },
+  { id: 2, name: 'The dark Knight', rating: 8 },
+  { id: 3, name: 'Hobbit', rating: 6 },
+];
+export const moviesList2: Movie[] = [
+  { id: 4, name: 'Love story', rating: 6.7 },
+  { id: 5, name: 'Zeland', rating: 8 },
+  { id: 6, name: 'Power Rangers', rating: 6 },
+];
