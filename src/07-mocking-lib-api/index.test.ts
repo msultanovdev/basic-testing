2eb20a828fe7793d8a5320215720152cd4acf430
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-02-12 21:29:12 +0500

    feat: solve mocking lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,52 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
 
-describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    // Write your test here
+describe('throttledGetDataFromApi', (): void => {
+  const baseURL = 'https://jsonplaceholder.typicode.com';
+  const api = '/posts/1';
+  beforeAll((): void => {
+    jest.useFakeTimers();
+  });
+  beforeEach((): void => {
+    jest.runOnlyPendingTimers();
+  });
+  afterEach((): void => {
+    jest.clearAllMocks();
+  });
+  afterAll((): void => {
+    jest.useRealTimers();
   });
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
+  test('should create instance with provided base url', async (): Promise<void> => {
+    const spyOnCreateInstance: jest.SpyInstance<AxiosInstance> = jest.spyOn(
+      axios,
+      'create',
+    );
+    await throttledGetDataFromApi(api);
+    expect(spyOnCreateInstance).lastCalledWith({ baseURL });
   });
 
-  test('should return response data', async () => {
-    // Write your test here
+  test('should perform request to correct provided url', async (): Promise<void> => {
+    const spyOnGet = jest.spyOn(axios.Axios.prototype, 'get');
+    await throttledGetDataFromApi(api);
+    expect(spyOnGet).lastCalledWith(api);
+  });
+
+  test('should return response data', async (): Promise<void> => {
+    type expectedDataTypes = {
+      userId: number;
+      id: number;
+      title: string;
+      body: string;
+    };
+
+    const expectedData: expectedDataTypes = {
+      userId: expect.any(Number),
+      id: expect.any(Number),
+      title: expect.any(String),
+      body: expect.any(String),
+    };
+    const res = await throttledGetDataFromApi(api);
+    expect(res).toEqual(expect.objectContaining(expectedData));
   });
 });
